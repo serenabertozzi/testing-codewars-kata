37e9f632c3276919117711589502264ce2396c3d
serenabertozzi <serenabertozzi@protonmail.com>
2021-12-22 17:40:42 +0100

    Additional test and refactoring

--- JavaScript/7kyu/chickens/chicken.js
+++ JavaScript/7kyu/chickens/chicken.js
@@ -1,15 +1,14 @@
 function chickenSexer(bobsDecisions, expertDecisions) {
   let result = 0;
 
-  for (let i = 0; i < bobsDecisions.length; i++) {
-    expertDecisions.forEach((answer) => {
-      if (bobsDecisions[i] === answer) {
-        result++;
-      } else if (bobsDecisions === "?" || answer === "?") {
-        (result = result + 0), 5;
-      }
-    });
-  }
+  bobsDecisions.forEach((bobAnswer, i) => {
+    if (bobAnswer === expertDecisions[i]) {
+      result++;
+    }
+    if (bobAnswer === "?" || expertDecisions[i] === "?") {
+      result = result + 0.5;
+    }
+  });
 
   return result;
 }
--- JavaScript/7kyu/chickens/chicken.test.js
+++ JavaScript/7kyu/chickens/chicken.test.js
@@ -2,16 +2,22 @@ const chickenSexer = require("./chicken.js");
 
 describe("chickenSexer", () => {
   it("given two equal answers, result is 1", () => {
-    expect(chickenSexer(["M"], ["M"])).toBe(1);
+    expect(chickenSexer([""M""], ["M"])).toBe(1);
   });
   it("given two different answers, result is 0", () => {
     expect(chickenSexer(["F"], ["M"])).toBe(0);
   });
 
   it("given one '?' answer, result is 0.5", () => {
-    expect(chickenSexer(["?"], ["M"])).toBe(0);
+    expect(chickenSexer(["?"], ["M"])).toBe(0.5);
   });
   it("given two equal arrays of answers, result is 2", () => {
     expect(chickenSexer(["M", "F"], ["M", "F"])).toBe(2);
+  });
+  it("given two equal arrays of answers, one containing '?', result is 1.5", () => {
+    expect(chickenSexer(["M", "F"], ["?", "F"])).toBe(1.5);
+  });
+  it("given ['M', '?', 'M'] and ['M', 'F', '?'] result is 2", () => {
+    expect(chickenSexer(["M", "?", "M"], ["M", "F", "?"])).toBe(2);
   });
 });
