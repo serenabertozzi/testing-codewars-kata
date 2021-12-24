b024a6a5009c4731d850a034718b7a94c8343388
serenabertozzi <serenabertozzi@protonmail.com>
2021-12-24 12:32:26 +0100

    Edge cases refactor (bouncing balls)

--- JavaScript/6kyu/bouncingBall/bouncingBall.js
+++ JavaScript/6kyu/bouncingBall/bouncingBall.js
@@ -3,7 +3,7 @@ const bouncingBall = (h, b, w) => {
   let firstBounce = h * b;
   let newH = h;
 
-  if (h <= 0 || b < 0 || b > 1 || w > h) {
+  if (h <= 0 || b <= 0 || b >= 1 || w >= h) {
     bounces = -1;
   } else if (firstBounce <= w) {
     bounces++;
